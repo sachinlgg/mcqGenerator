b6e79a109800a7f90a2a802b4488ef518894df98
Sachin <sachin.plaksha@gmail.com>
2024-07-02 13:50:13 +0530

    update logger.py

--- src/mcqgenerator/logger.py
+++ src/mcqgenerator/logger.py
@@ -2,7 +2,7 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f'{datetime.now().strftime('%m'_%d_%Y_%H_%M_%S)}.log'
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 log_path = os.path.join(os.getcwd(), 'logs')
 
@@ -10,7 +10,7 @@ os.makedirs(log_path, exist_ok=True)
 
 LOG_FILEPATH = os.path.join(log_path, LOG_FILE)
 
-logging(
+logging.basicConfig(
     level=logging.INFO,
     filename=LOG_FILEPATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
